e08495cde954953a9e364741f2681da713b2c2f9
jocypher <arthurwilchield@gmail.com>
2026-06-08 13:28:00 +0000

    feat: integrate winston for logging and add morgan middleware for request logging

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,46 @@
 import express from "express";
 import "reflect-metadata";
 import AppDatasource from "./db/datasource";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import appRouter from "./api/index";
 import { requestLogger } from "./core/middlewares/reqLoggerMiddlewares";
 import { EthereumListenerService } from "./core/services/ethereum/ethereumListenerService";
-import { seedAlertTypes,getCachedAlertTypes } from "./db/seeds/alertType.seed";
+import { seedAlertTypes, getCachedAlertTypes } from "./db/seeds/alertType.seed";
 import client from "./core/config/redisConfig";
-import "./core/workers"
+import "./core/workers";
 import morgan from "morgan";
 import helmet from "helmet";
+import morganMiddleware from "./core/middlewares/morganMiddleware";
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-app.use(helmet())
-app.use(express.json())
-app.use(morgan("dev"))
+app.use(helmet());
+app.use(express.json());
+app.use(morganMiddleware)
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(requestLogger)
+app.use(requestLogger);
 
-app.use(appRouter)
-
+app.use(appRouter);
 
 AppDatasource.initialize()
   .then(async () => {
     console.log("Database connected successfully");
     await client.connect();
-    
+
     await seedAlertTypes();
 
     await getCachedAlertTypes();
-    
-    
-    app.listen(PORT, async() => {
+
+    app.listen(PORT, async () => {
       console.log(`Server is running on port ${PORT}`);
       console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
-      const ethereumService = new EthereumListenerService()
+      const ethereumService = new EthereumListenerService();
       //await ethereumService.startListening()
     });
-   
   })
   .catch((error) => {
     console.error("Error connecting to database:", error);
